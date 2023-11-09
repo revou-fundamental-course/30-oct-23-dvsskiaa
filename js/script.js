6212975acfe15863a9d9ed125d22536245ecc854
Saskia Maya Adiva <80552244+dvsskiaa@users.noreply.github.com>
2023-11-09 23:31:34 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var gender = document.getElementById("radio")
+
+var usia = document.getElementById("box-usia");
+var bb = document.getElementById("box-bb");
+var tb = document.getElementById("box-tb");
+
+if (gender === "laki-laki") {
+
+}
